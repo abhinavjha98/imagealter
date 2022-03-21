61fb7c5467a254322c4682bad00b87c1fdd1eb51
Abhinav Jha <abhinavjha@AJ.local>
2022-03-21 12:39:48 +0530

    Minor changes

--- ela/utils.py
+++ ela/utils.py
@@ -16,29 +16,27 @@ from keras.preprocessing.image import ImageDataGenerator
 import os
 from PIL import Image, ImageChops, ImageEnhance
 
-def ELA(img_path):
-    """Performs Error Level Analysis over a directory of images"""
-    
-    TEMP = 'ela_' + 'temp.jpg'
-    SCALE = 10
-    original = Image.open(img_path)
-    try:
-        original.save(TEMP, quality=90)
-        temporary = Image.open(TEMP)
-        diff = ImageChops.difference(original, temporary)
-        
-    except:
-        
-        original.convert('RGB').save(TEMP, quality=90)
-        temporary = Image.open(TEMP)
-        diff = ImageChops.difference(original.convert('RGB'), temporary)
-        
-       
-    d = diff.load()
-    
-    WIDTH, HEIGHT = diff.size
-    for x in range(WIDTH):
-        for y in range(HEIGHT):
-            d[x, y] = tuple(k * SCALE for k in d[x, y])
+image_size = (128,128)
 
-    return diff
+def ELA(path,quality):
+    temp_filename = 'temp_file_name.jpg'
+    ela_filename = 'temp_ela.png'
+    path_data = path.split("/")
+    image = Image.open('media/'+path_data[2]).convert('RGB')
+    image.save(temp_filename, 'JPEG', quality = quality)
+    temp_image = Image.open(temp_filename)
+    
+    ela_image = ImageChops.difference(image, temp_image)
+    
+    extrema = ela_image.getextrema()
+    max_diff = max([ex[1] for ex in extrema])
+    if max_diff == 0:
+        max_diff = 1
+    scale = 255.0 / max_diff
+    
+    ela_image = ImageEnhance.Brightness(ela_image).enhance(scale)
+    
+    return ela_image
+
+# def prepare_image(image_path):
+#     return np.array(ELA(image_path,90).resize(image_size)).flatten()/255.0
--- ela/views.py
+++ ela/views.py
@@ -3,6 +3,10 @@ from django.core.files.storage import FileSystemStorage
 
 from ela.utils import ELA
 # Create your views here.
+from PIL import Image 
+import base64
+from io import BytesIO
+
 def index(request):
     if request.method == 'POST' and request.FILES['upload']:
         upload = request.FILES['upload']
@@ -10,8 +14,17 @@ def index(request):
         file = fss.save(upload.name, upload)
         file_url = fss.url(file)
         print(file_url)
-        ELA(file_url)
-        return render(request, 'index.html', {'file_url': file_url})
+        image = ELA(file_url,90)
+        # img = Image.fromarray(image, 'RGB')
+        # data = BytesIO()
+        # img.save('test.png') # pick your format
+        # data64 = base64.b64encode(data.getvalue())
+        # img_file = u'data:img/jpeg;base64,'+data64.decode('utf-8') 
+        print(image)
+        image.save('media/test.png')
+        img_url = fss.url('test.png')
+        print(img_url)
+        return render(request, 'index.html', {'file_url': file_url,'img_file':img_url})
     return render(request, 'index.html')
 
 
