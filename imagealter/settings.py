b11547ccaeec77dbee3404761a6bba512c0f2a32
Abhinav Jha <abhinavjha@AJ.local>
2022-03-21 12:40:47 +0530

    Changed

--- imagealter/settings.py
+++ imagealter/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'bm9)1#8%p3fyqq6*$ck@#m6ense5%yes1i*ghh@0yt+#ya%)4y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
